7422911f2609e009ab3a027014785948300b2e95
Naji Ibrahim <najiibrahim1998@gmail.com>
2023-05-06 18:36:08 +0300

    third  commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+var cors = require('cors')
 const PORT = process.env.PORT || 3030;
 
 // your code
-
+app.use(cors())
 app.get('/',(req,res)=>{
     res.send('hi all')
 })
